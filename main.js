5dda8cd33901911a42c036b31a489878f21b1e1a
Kevin Lau <thekevlau@gmail.com>
2015-05-09 12:18:37 -0400

    li injection, basic save function framework

--- main.js
+++ main.js
@@ -2,12 +2,26 @@ var trailExtension = angular.module('trail-extension',['ngResource']);
 
 trailExtension.controller('popupController', ['$scope', '$http', function($scope, $http) {
 	$scope.loggedin = true;
-	$scope.trails = [{name: 'test'}];
-
+	$scope.trails = [
+        {name: 'test'},
+        {name: 'test2'},
+        {name: 'test3'}
+    ];
 
 	chrome.tabs.getSelected(null, function(tab) {
 		$scope.$apply(function () {
 	    	$scope.url = tab.url;
 	    });
 	});
+
+    $scope.saveStep = function(id) {
+        // comments if time
+        $http.post('/someurl', {'type' : "url", 'data' : $scope.url, 'trailid' : id}).
+        success(function(data, status, headers, config) {
+            //yay
+        }).
+        error (function(data, status, headers, config) {
+            //uhoh
+        });
+    }
 }]);
